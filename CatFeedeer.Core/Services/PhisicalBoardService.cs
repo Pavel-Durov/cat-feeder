806fd99d6a90f86bfd549f5f212515c4e3b0bc3b
Pavel Durov <cccpiscool@gmail.com>
2015-11-21 20:33:53 +0200

    Core - first models and such

---
+++ CatFeedeer.Core/Services/PhisicalBoardService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace CatFeedeer.Core.Services
+{
+    class PhisicalBoardService
+    {
+        public PhisicalBoardService()
+        {
+            _gpioController = Windows.Devices.Gpio.GpioController.GetDefault();
+        }
+
+        GpioController _gpioController;
+
+        #region ACTIVE PINS
+
+        public const int MOTOR_PIN = 10;
+
+        #endregion
+
+
+        public void SignalPin(int pinId, GpioPinValue value, GpioPinDriveMode mode = GpioPinDriveMode.Output)
+        {
+            var pin = _gpioController.OpenPin(pinId);
+            pin.Write(value);
+            pin.SetDriveMode(mode);
+        }
+
+        ///....
+    }
+}
